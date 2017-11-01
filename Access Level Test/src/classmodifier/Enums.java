c35526b12f582cd29cc373b1f7d7abfc1c0ec4e7
hashnet <maidul.hasan@gmail.com>
2017-11-01 13:03:52 +0400

    Commented the parts generating errors.

--- Access Level Test/src/classmodifier/Enums.java
+++ Access Level Test/src/classmodifier/Enums.java
@@ -5,9 +5,9 @@ package classmodifier;
 enum EnumsDefault {}
 //private enum EnumsPrivate {}		//Illegal modifier for the enum EnumsPrivate; only public is permitted
 
-abstract enum EnumsAbstract {}		//Illegal modifier for the enum EnumsAbstract; only public is permitted
-final enum EnumsFinal {}			//Illegal modifier for the enum EnumsFinal; only public is permitted
-abstract final enum EnumsAbsFinal {}//Illegal modifier for the enum EnumsAbsFinal; only public is permitted
+//abstract enum EnumsAbstract {}		//Illegal modifier for the enum EnumsAbstract; only public is permitted
+//final enum EnumsFinal {}			//Illegal modifier for the enum EnumsFinal; only public is permitted
+//abstract final enum EnumsAbsFinal {}//Illegal modifier for the enum EnumsAbsFinal; only public is permitted
 
 //public abstract enum Enums {}		//Illegal modifier for the enum Enums; only public is permitted
 //public final enum Enums {}			//Illegal modifier for the enum Enums; only public is permitted
